1dd6a345559727cc9d0fa66612aa25e492a14b94
Oleksandr Shevchyk <shevchuk-ole@i.ua>
2017-05-09 15:55:33 +0300

    First commit for JS

---
+++ 20170508-js-basics/ClassExample.js
@@ -0,0 +1,13 @@
+// class MyClass {
+//
+// }
+
+var MyClass = (function () {
+	function MyClass() {
+
+	}
+
+	return MyClass;
+})();
+
+var a = new MyClass();
---
+++ 20170508-js-basics/syntax.js
@@ -0,0 +1,18 @@
+var
+	a = 1,
+	b = 2;
+
+const z = 10;
+var MY_CONST = 21;
+// Comment
+/*
+	Comment
+*/
+console.log(a);
+//
+// 1 1 1 0 & | ^
+// 1 0 1 1
+//
+// if (true) {
+// 	let localScopeVar = 10;
+// }
